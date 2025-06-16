4916eedd06424096e36f9bd3ca59c811b1899def
kainat <kainatfaisal50@gmail.com>
2025-06-16 23:37:47 +0500

    jwt-auth

---
+++ ai-ticket-assistant/controllers/user.js
@@ -0,0 +1,68 @@
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import { inngest } from "../inngest/client.js";
+
+export const signup = async (req, res) => {
+  const { email, password, skills = [] } = req.body;
+  try {
+    const hashed = brcypt.hash(password, 10);
+    const user = await User.create({ email, password: hashed, skills });
+
+    //fire inngest event
+
+    await inngest.send({
+      name: "user/signup",
+      data: {
+        email,
+      },
+    });
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: "Signup failed", details: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = User.findOne({ email });
+    if (!user) return res.status(401).json({ error: "User not found" });
+
+    const isMatch = await brcypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed", details: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return res.status(401).json({ error: "Unauthorized" });
+    });
+    res.json({ message: "Logout successful!" });
+  } catch (error) {
+    res.status(500).json({ error: "Logout failed", details: error.message });
+  }
+};
